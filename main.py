7722f32cafe3cac175c26a87a80499b7746dc7c5
Eduardo T. Vanin <1134891@atitus.edu.br>
2023-06-24 20:03:14 -0300

    Manipulação das marcações.

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
+
 import pygame
+import tkinter as tk
+from tkinter import messagebox, simpledialog
 
-pygame.init()
+# Funções de manipulação de marcações
+def save_marks():
+    try:
+        with open("marks.txt", "w") as file:
+            for name, pos in marks:
+                file.write(f"{name},{pos[0]},{pos[1]}\n")
+            if unknown_point is not None:
+                file.write(f"desconhecido,{unknown_point[0]},{unknown_point[1]}\n")
+        messagebox.showinfo("Mensagem", "As marcações foram salvas com sucesso.")
+    except Exception as e:
+        messagebox.showerror("Erro", f"Ocorreu um erro ao salvar as marcações:\n{str(e)}")
 
-# Configurações da janela
-window_width = 800
-window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Space Marker Eduardo e Anderson")
+def load_marks():
+    global unknown_point
+    try:
+        with open("marks.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                name, x, y = line.strip().split(",")
+                if name.lower() == "desconhecido":
+                    unknown_point = (int(x), int(y))
+                else:
+                    marks.append((name, (int(x), int(y))))
+        messagebox.showinfo("Mensagem", "As marcações foram carregadas com sucesso.")
+    except FileNotFoundError:
+        messagebox.showinfo("Mensagem", "Arquivo de marcações não encontrado.")
+    except Exception as e:
+        messagebox.showerror("Erro", f"Ocorreu um erro ao carregar as marcações:\n{str(e)}")
 
-icon_image = pygame.image.load("space.png")
-pygame.display.set_icon(icon_image)
+def clear_marks():
+    global unknown_point
+    marks.clear()
+    unknown_point = None
+    messagebox.showinfo("Mensagem", "Todas as marcações foram removidas.")
 
-# Carregar imagem de fundo
-background_image = pygame.image.load("spacemarker.jpg")
-background_image = pygame.transform.scale(background_image, (window_width, window_height))
-
-# Configurações do círculo e das marcações
-circle_radius = 5
-marks = []  # Lista para armazenar as marcações (nome e coordenadas)
-unknown_point = None  # Coordenadas do ponto "desconhecido"
-
-font = pygame.font.Font(None, 20)
-text_color = (255, 255, 255)
-message_1 = "Pressione C para carregar os pontos"
-message_2 = "Pressione E para excluir os pontos"
-message_3 = "Pressione S para salvar os pontos"
+def calculate_distance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    distance = abs(x2 - x1) + abs(y2 - y1)
+    return distance
